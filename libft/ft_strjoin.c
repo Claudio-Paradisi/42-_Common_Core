028d46363ad2eea21f2497285c1ed71ffb64b6ee
claudio paradisi <claudio.paradisi94@gmail.com>
2024-12-14 21:35:30 +0100

    ft_strjoin

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -12,32 +12,22 @@
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(const char *s1, const char *s2)
 {
 	char	*buffer;
-	unsigned int 	len;
-	unsigned int	i;
+	size_t 	len;
+	size_t	i;
 	
 	i = 0;
 	len = ft_strlen(s1) + ft_strlen(s2);
 	buffer = (char *)malloc((len + 1) * sizeof(char));
 	if (!buffer)
 		return (NULL);
-	while (i++ < ft_strlen(s1))
+	while (i < ft_strlen(s1))
+	{
 		buffer[i] = s1[i];
-	i = 0;
-	while((ft_strlen(s1) + i++) < len)
-		buffer[ft_strlen(s1) + i] = s2[i];
-	buffer[len] = '\0';
-	return  (buffer);
-}
-
-int main(int ac, char **av)
-{
-	if (ac > 1){
-		char *ciao = ft_strjoin(av[1], av[2]);
-		printf("%s\n", ciao);
-		free(ciao);
+		i++;
 	}
-		return 0;
+	ft_strlcat(buffer, s2, (len + 1));
+	return  (buffer);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -17,16 +17,17 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int	ft_isalpha (char c);
-int	ft_isdigit (char c);
-int	ft_isalnum (char c);
-int	ft_isascii (char c);
-int	ft_isprint (char c);
-void	*ft_memset (void *s, int c, size_t n);
-void	ft_bzero (void *s, size_t n);
-void	*ft_memcpy (void *dest, const void *src, size_t n);
-void	*ft_memmove (void *dest, const void *src, size_t n);
-size_t	ft_strlen (const char *str);
-size_t	ft_strlcpy (char *dest, const char *src, size_t size);
+int	ft_isalpha(char c);
+int	ft_isdigit(char c);
+int	ft_isalnum(char c);
+int	ft_isascii(char c);
+int	ft_isprint(char c);
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+size_t	ft_strlen(const char *str);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
 
 #endif
